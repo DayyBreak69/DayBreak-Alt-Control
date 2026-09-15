4056b418bb1896b65bea5eceb17aeaea383a8f10
DayyBreak69 <terranjones870@gmail.com>
2026-09-15 00:24:12 -0500

    Update URL loading mechanism for DayBreakAltControl

--- Loader.lua
+++ Loader.lua
@@ -9,4 +9,5 @@ getgenv().Settings = {
     }
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DayyBreak69/DayBreak-Alt-Control/main/DayBreakAltControl.lua"))()
+local url = "https://raw.githubusercontent.com/DayyBreak69/DayBreak-Alt-Control/main/DayBreakAltControl.lua?t=" .. tostring(os.time())
+loadstring(game:HttpGet(url))()
