d4a31ce7d2f86cc26cf54b0168c1eed6cc00f2f7
DayyBreak69 <terranjones870@gmail.com>
2026-09-14 23:15:20 -0500

    Update DayBreakAltControl.lua

--- DayBreakAltControl.lua
+++ DayBreakAltControl.lua
@@ -25,7 +25,7 @@ getgenv().Settings = {
     -- MUSIC BOT
     musicPrefix         = "/",
     musicBotAccount     = "",
-    musicServerURL      = "http://127.0.0.1:5000",
+    musicServerURL      = "",
     musicApiKey         = "",
     musicGlobalCooldown = 3,
     musicPlayCooldown   = 10,
